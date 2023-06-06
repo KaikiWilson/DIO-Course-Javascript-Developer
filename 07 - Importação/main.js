99023ce6637568d6e2b974fa9fd4c2006d291b4d
Kaiki Wilson <kaikiwilson7@gmail.com>
2023-06-06 18:06:30 -0300

    Adicionado novas pastas com atividades feitas!

---
+++ 07 - Importação/main.js
@@ -0,0 +1,19 @@
+const { gets, print } = require('./funcoes-auxiliares');
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print(maiorValor);
