b4ea4370c268710136b1249bf26b043136fa6251
Kaiki Wilson <kaikiwilson7@gmail.com>
2023-06-06 18:07:36 -0300

    Enviando o restante das atividades e modificações!

--- 05 - Criando Objetos e Classes em JavaScript/atv.js
+++ 05 - Criando Objetos e Classes em JavaScript/atv.js
@@ -5,9 +5,26 @@
     em reais para realizar esse percurso.
 */
 
-class carros {
+class Carro {
     marca;
     cor;
     gastoMedioPorKm;
+
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+
+    calcularGastoDePercurso(distanciaEmKm, precoCombustivel) {
+        return distanciaEmKm * this.gastoMedioPorKm * precoCombustivel;
+    }
+
 }
 
+const uno = new Carro('Fiat', 'Prata', 1/12);
+const palio = new Carro('Fiat', 'Preta', 1/10);
+palio.calcularGastoDePercurso(70,5);
+console.log(palio.calcularGastoDePercurso(70,5));
+
+//OUTRA ATIVIDADE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
---
+++ 06 - Arrays e Estruturas de repetição/atv.js
@@ -0,0 +1,20 @@
+
+
+
+const numero = 4;
+for (let i = 1; i < 10; i++) {
+    console.log(i * numero);
+    
+}
+
+//2 ATIVIDADE
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+    
+}
