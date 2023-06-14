9907aab88632f610af3659840c0082a07cd69adb
Kaiki Wilson <kaikiwilson7@gmail.com>
2023-06-13 21:30:59 -0300

    Envio de arquivos pendentes pra comitar quando abri o vscode

--- DIO-Course-Javascript-Developer/08 - Avaliação/exercicio2.js
+++ DIO-Course-Javascript-Developer/08 - Avaliação/exercicio2.js
@@ -16,23 +16,23 @@
 const { gets, print } = require('./funcoes-auxiliares-ex2')
 
 const n = gets();
-let maiorNumeroPar = 0;
-let menorNumeroImpar = 0;
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
 
 for (let i = 0; i < n; i++) {
     const numero = gets();
+
     if (numero % 2 === 0) {
-        if (numero > maiorNumeroPar) {
-        maiorNumeroPar = numero;
+        if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+            maiorNumeroPar = numero;
         }
     } else {
-        if (menorNumeroImpar === 0) {
-            menorNumeroImpar = numero;
-        } else if(numero < menorNumeroImpar) {
+        if (menorNumeroImpar === null || numero < menorNumeroImpar) {
             menorNumeroImpar = numero;
         }
     }
     
 }
 
-print(maiorNumeroPar);
+print('Maior número par: ' + maiorNumeroPar);
+print('Menor número impar: ' + menorNumeroImpar);
---
+++ DIO-Course-Javascript-Developer/08 - Avaliação/exercicio3.js
@@ -0,0 +1,49 @@
+// 1) Faça um programa que calcule e imrpima o salário a ser transferido para um funcionário.
+// Para realizar o calculo receba o valor bruto do salário e o adicional dos beneficios.
+// O salário a ser transferido é calculado da seguinte maneira:
+
+// valor bruto do salario - percentual de imposto mediante a faixa salarial + adicional dos beneficios.
+
+// Para calcular o percentual de imposto segue:
+
+// De R$ 0.00 a R$ 1100.00 = 5.00%
+// De R$ 1100.01 a R$ 2500.00 = 10.00%
+// Maior que R$ 2500.00 = 15.00%
+
+/* Exemplo:
+    Entrada:
+        2000
+        250
+
+    Saída:
+        2050.00
+*/
+
+const { gets, print } = require('./funcoes-auxiliares-ex3');
+
+const valorSalario = gets();
+const valorBeneficios = gets();
+
+function calcularPorcentagem(valor, percentual) {
+    return valor * (percentual / 100);
+}
+
+function pegarAliquota(salario) {
+
+    //De R$ 0.00 a R$ 1100.00 = 5.00%
+    //De R$ 1100.01 a R$ 2500.00 = 10.00%
+    //Maior que R$ 2500.00 = 15.00%
+
+    if (salario >= 0 && salario <= 1100) {
+        return 5;
+    } else if (salario >= 1100.01 && salario <= 2500) {
+        return 10;
+    } else {
+        return 15;
+    }
+}
+const aliquotaImposto = pegarAliquota(valorSalario);
+const valorImposto = (calcularPorcentagem(valorSalario, aliquotaImposto));
+const valorATrasferir = valorSalario - valorImposto + valorBeneficios;
+
+print(valorATrasferir);
